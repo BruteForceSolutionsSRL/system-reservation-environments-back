b5afe4695c6b05b0848988db3cef822007b96fd2
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-03-29 18:19:55 -0400

    refactor: classroom assignment code refactored, ready to test!

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -26,35 +26,13 @@ class ReservationController extends Controller
         return response()->json([], 200); 
     }
 
-    public function ayuda(Request $request) 
-    {
-        $id = $request->input('id'); 
-        $id = 1;
-        $reservation = Reservation::find($id);
-        $horarios = $reservation->timeSlotReservations;
-        $piv = -1; 
-        $otro = -1;
-        foreach ($horarios as $horario) {
-            if ($piv==-1) $piv = $horario->timeSlot->id; 
-            else $otro = $horario->timeSlot->id;
-        }
-
-        $classrooms = $reservation->reservationClassrooms;
-
-        $classroomId = 3; 
-        $date = '2024-03-29';
-
-        $result = $this->findAcceptedReservationsByClassroomAndDate(3, '2024-03-29');         
-        $reservationStatuses = ReservationStatus::all();
-        $reservationStatus = 0;
-        foreach($reservationStatuses as $reservationStatusIterator) {
-            if ($reservationStatusIterator->status=='accepted') 
-                $reservation_status = $reservationStatusIterator->id;
-        } 
-
-        return response()->json([$result, $reservationStatus], 200);
-    }
-    public function accept(Request $request) 
+    /**
+     * @description: 
+     * Endpoint to assign reservations 
+     * if fulfill no overlapping with assigned 
+     * reservations.
+     */
+    public function assign(Request $request) 
     {
         try {
 
@@ -72,7 +50,8 @@ class ReservationController extends Controller
 
             $ok = $this->checkAvailibility($reservation); 
             if ($ok==false) {
-                return response()->json(['message'=> 'Already occupied classroom(s)'], 200);
+                return response()
+                        ->json(['message'=> 'Already occupied classroom(s)'], 200);
             }
 
             $acceptedStatus = ReservationStatus::find(1);
@@ -87,7 +66,8 @@ class ReservationController extends Controller
 
     /**
      * @description: 
-     * Function to check availability for all classrooms to do a reservation in a step 
+     * Function to check availability for all classrooms to do a 
+     * reservation in a step 
      */
     private function checkAvailibility($reservation): bool
     {
@@ -119,7 +99,8 @@ class ReservationController extends Controller
                 foreach($reservations as $reservation) {
                     $left = -1; 
                     $right = -1;
-                    foreach ($reservation->timeSlotReservations as $timeSlotIterable) {
+                    $timeSlotReservations = $reservation->timeSlotReservations;
+                    foreach ($timeSlotReservations as $timeSlotIterable) {
                         if ($left==-1) $left = $timeSlotIterable->timeSlot->id; 
                         else $right  = $timeSlotIterable->timeSlot->id;
                     } 
@@ -128,7 +109,7 @@ class ReservationController extends Controller
                         $left = $right; 
                         $right = $temp; 
                     }
-                    $ok &= ($right>$init) || ($left>$end); 
+                    $ok &= ($right<=$init) || ($left>=$end); 
                 }
                 if ($ok==false) {
                     return false;                     
@@ -142,54 +123,40 @@ class ReservationController extends Controller
         return true; 
     }
 
-    protected $sql = ''; 
-    private function findAcceptedReservationsByClassroom(Classroom $classroom) 
-    {
-        $classId = $classroom->id;
-        $result = Reservation::with('timeslots', 'classrooms', 'status', 'group')
-        ->whereHas('classrooms', function ($query) use ($classId) {
-            $query->where('classroom.id', $classId);
-        })
-        ->where('reservation_status.value', 'accepted')
-        ->get()
-        ->toArray();
-    
-        return $result;
-    }
     /**
      * @description: 
      * Function to retrieve all reservations with equal date and classroom  
      */
-    private function findAcceptedReservationsByClassroomAndDate($classroomId, $date)
+    private function findAcceptedReservationsByClassroomAndDate($classroomId, $date) 
     {
-        $result = array(); 
+        $acceptedStatusId = ReservationStatus::where('status', 'accepted')
+                                ->get()
+                                ->pop()
+                                ->id; 
 
-        $reservations = Reservation::all();
-        $goalDate = new DateTime($date);
-        
-        foreach ($reservations as $reservation) {
-            $reservationStatus = $reservation->reservationStatus->status;
-            if ($reservationStatus!='accepted') continue;
+        $reservationSet = Reservation::with('reservationClassrooms')
+                        ->whereHas('reservationClassrooms', 
+                            function ($query) use ($classroomId) {
+                                $query -> where ('classroom_id', '=', $classroomId);
+                            })
+                        ->where('reservation_status_id', $acceptedStatusId) 
+                        ->where(function($query) use ($date) {
+                            $query->where('date', '=', $date)
+                                    ->orWhere('repeat', '>', 0);
+                        })
+                        ->get();
+        $result = array();
 
-            $classroomBelong = false; 
-            $classrooms = $reservation->reservationClassrooms;
-            foreach ($classrooms as $classroom) 
-                $classroomBelong |= ($classroom->classroom_id)==$classroomId;
-
-            if ($classroomBelong==false) continue;
-
-            $reservationDate = $reservation->date;
-            $reservationRepeat = $reservation->repeat;
-            if ($reservationDate!=$date && $reservationRepeat<=0) continue;
-
-            if ($reservation->repeat>0) {
+        foreach ($reservationSet as $reservation) {
+            if ($reservation->repeat==0) array_push($result, $reservation);
+            else {
                 $initialDate = new DateTime($reservation->date);
-                if ($initialDate>$goalDate) continue;
-                
+                $goalDate = new DateTime($date);
+
                 $repeat = $reservation->repeat;
-                $diff = $initialDate->diff($goalDate)->days; 
-                if ($diff%$repeat==0) array_push($result, $reservation);
-            } else array_push($result, $reservation); 
+                $difference = $initialDate->diff($goalDate)->days;
+                if ($difference%$repeat==0) array_push($result, $reservation);
+            }
         }
 
         return $result;
