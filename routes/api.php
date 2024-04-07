57bc4228c8ee4231b534269b4c31735a99898a6b
William Calle <cwilliam14c@gmail.com>
2024-04-07 11:35:15 -0400

    feat(update-routes): Renamed endpoints and a correctly grouped method

--- routes/api.php
+++ routes/api.php
@@ -26,34 +26,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::middleware('api:cors')->group(function() {
-    // Route::post('reservation/assign', 'App\Http\Controllers\ReservationController@assign');
-    // Route::middleware('sanitize:api')->post('/classroom', 'App\Http\Controllers\ClassroomController@store');
-    // Route::get('/classroomtypes', 'App\Http\Controllers\ClassroomTypeController@list');
-    // Route::get('/classrooms/block/{blockId}','App\Http\Controllers\ClassroomController@classroomsByBlock');
-    // Route::get('/classroom', 'App\Http\Controllers\ClassroomController@list');
-//});
-
-// Route::get('reservations', 'App\Http\Controllers\ReservationController@index');
-// Route::get('reservation/{reservationId}', 'App\Http\Controllers\ReservationController@show');
-// Route::middleware('sanitize:api')->put('reservation/reject/{reservationId}', 'App\Http\Controllers\ReservationController@rejectReservation');
-
-// Route::middleware('sanitize:api')->post('notification', 'App\Http\Controllers\NotificationController@store');
-
-// Route::get('/subjects/teacher/{teacherId}', [TeacherSubjectController::class, 'subjectsByTeacher']);
-// Route::get('/teachers/subject/{universitySubjectID}', [TeacherSubjectController::class, 'teachersBySubject']);
-
 Route::controller(ReservationController::class)->group(function() {
-    Route::post('/reservation/assign', 'assign');
+    Route::middleware('sanitize:api')->post('/reservation/assign', 'assign');
     Route::get('/reservations', 'index');
     Route::get('/reservation/{reservationId}', 'show');
     Route::middleware('sanitize:api')->put('/reservation/reject/{reservationId}', 'rejectReservation');
+    Route::middleware('sanitize:api')->post('/reservation', 'store');
 });
 
 Route::controller(ClassroomController::class)->group(function() {
     Route::middleware('sanitize:api')->post('/classroom', 'store');
     Route::get('/classrooms/block/{blockId}','classroomsByBlock');
-    Route::get('/classroom', 'list');
+    Route::get('/classrooms', 'list');
 });
 
 Route::controller(TeacherSubjectController::class)->group(function() {
@@ -65,4 +49,4 @@ Route::get('/classroomtypes', [ClassroomTypeController::class, 'list']);
 Route::middleware('sanitize:api')->post('/notification', [NotificationController::class ,'store']);
 Route::get('/blocks', [BlockController::class, 'list']);
 Route::get('/timeslots', [TimeSlotController::class, 'list']);
-Route::post('/reservation', [ReservationController::class, 'store']);
+
