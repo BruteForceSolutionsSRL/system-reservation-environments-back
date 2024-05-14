6caeeda198b3e9b457d9537b99aa3a4dbfd91d60
Emerson <67840383+Emerson-DVV@users.noreply.github.com>
2024-05-14 00:02:01 -0400

    feat: classroom log and trigger

--- database/migrations/2024_03_25_204146_create_classrooms_table.php
+++ database/migrations/2024_03_25_204146_create_classrooms_table.php
@@ -38,6 +38,94 @@ class CreateClassroomsTable extends Migration
                     ->on('classroom_statuses')
                     ->cascadeOnDelete();
         });
+        DB::unprepared('
+            create trigger classroom_insert after insert on classrooms
+            for each row
+            begin
+            declare v_block_name varchar (255);
+            declare v_classroom_type_name varchar (255);
+            declare v_classroom_status_name varchar (255);
+        
+            select 
+                B.name,
+                CT.description,
+                CS.name
+            into
+                v_block_name,
+                v_classroom_type_name,
+                v_classroom_status_name
+            from 
+                blocks B,
+                classroom_types CT, 
+                classroom_statuses CS
+            where 
+                new.block_id = B.id 
+                and new.classroom_type_id = CT.id
+                and new.classroom_status_id = CS.id;
+        
+            insert into classroom_logs (
+                classroom_id, 
+                name, 
+                capacity, 
+                floor, 
+                block_name,
+                classroom_type_name,
+                classroom_status_name) 
+            values (
+                new.id,
+                new.name,
+                new.capacity,
+                new.floor,
+                v_block_name,
+                v_classroom_type_name,
+                v_classroom_status_name
+            );
+            end;
+        ');
+        DB::unprepared('
+            create trigger classroom_update after update on classrooms
+            for each row
+            begin
+            declare v_block_name varchar (255);
+            declare v_classroom_type_name varchar (255);
+            declare v_classroom_status_name varchar (255);
+        
+            select 
+                B.name,
+                CT.description,
+                CS.name
+            into
+                v_block_name,
+                v_classroom_type_name,
+                v_classroom_status_name
+            from 
+                blocks B,
+                classroom_types CT, 
+                classroom_statuses CS
+            where 
+                new.block_id = B.id 
+                and new.classroom_type_id = CT.id
+                and new.classroom_status_id = CS.id;
+        
+            insert into classroom_logs (
+                classroom_id, 
+                name, 
+                capacity, 
+                floor, 
+                block_name,
+                classroom_type_name,
+                classroom_status_name) 
+            values (
+                new.id,
+                new.name,
+                new.capacity,
+                new.floor,
+                v_block_name,
+                v_classroom_type_name,
+                v_classroom_status_name
+            );
+            end;
+        ');
     }
 
     /**
@@ -48,5 +136,11 @@ class CreateClassroomsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('classrooms');
+        DB::unprepared('
+            drop trigger if exists classroom_insert;
+        ');
+        DB::unprepared('
+            drop trigger if exists classroom_update;
+        ');
     }
 }
