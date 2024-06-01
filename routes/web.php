a5b6759695a9533c4b5b8eff352315a3f3818d7f
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-05-31 21:47:15 -0400

    mail

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/send', function () {
+    $details = [
+        'title' => 'Mail from Codersvibe.com',
+        'body' => 'This is my first mail using Gmail SMTP'
+    ];
+   
+    \Mail::to('202103261@est.umss.edu')->send(new \App\Mail\NotificationMail($details));
+
+    dd('Email is sent. BD');
+});
