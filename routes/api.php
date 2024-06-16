b7c97aed7e5330077efcb0c35ee46b97fed5faba
Emerson <67840383+Emerson-DVV@users.noreply.github.com>
2024-06-16 13:27:50 -0400

    feat: routes and notifications

--- routes/api.php
+++ routes/api.php
@@ -34,10 +34,15 @@ use App\Http\Controllers\{
  * Permisos
  * request_reserve => solicitud de reserva (docente)
  * reservation_handling => atender solicitudes (administrador)
- * availability => ver disponibilidad (all)
+ * availability => ver disponibilidad (all) Mejor lo quitamos no tiene sentido protegerlo
  * classrooms_statistics => ver estadisticas de ambientes (all)
- * notify => notificaciones solo se protege el post del controlador (all) 
+ * notify => notificaciones solo se protege el post del controlador (ENCARGADO) 
  * report => generar reportes de uso de ambiente (Administrador) 
+ * environment_register => registrar ambiente (ENCARGADO) 
+ * environment_update => editar ambiente (ENCARGADO) 
+ * environment_remove => eliminar ambiente (ENCARGADO)
+ * reservation_cancel => cancelar solicitues de reserva (DOCENTE)
+ * history => Obtener el historia de solicitudes (DOCENTE)
  */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
@@ -71,17 +76,17 @@ Route::controller(ReservationController::class)->group(function() {
     Route::middleware('jwt.verify')->get('/reservations', 'list');
     Route::middleware('jwt.verify:reservation_handling')->get('/reservations/pending', 'getPendingRequests');
     Route::middleware('jwt.verify')->get('/reservations/teacher/{teacherId}/open', 'listRequestsByTeacher');
-    Route::middleware('jwt.verify')->get('/reservations/teacher/{teacherId}', 'listAllRequestsByTeacher');
-    Route::middleware('jwt.verify:history')->get('/reservations/history', 'getAllRequestsExceptPending');
+    Route::middleware('jwt.verify:reservation_cancel')->get('/reservations/teacher/{teacherId}', 'listAllRequestsByTeacher');
+    Route::middleware('jwt.verify:history')->get('/reservations/history', 'getAllRequestsExceptPending'); // Obsoleto
     Route::middleware('jwt.verify:history')->get('/reservations/history/teacher/{teacherId}', 'getAllRequestsExceptPendingByTeacher');
     Route::middleware('jwt.verify:report')->get('/reservations/reports', 'getReports');
     Route::middleware('jwt.verify')->get('/reservations/{reservationId}', 'show');
     Route::middleware('jwt.verify:reservation_handling')->get('/reservations/{reservationId}/conflicts', 'getConflicts');
-    Route::middleware('jwt.verify')->get('/reservations/classroom/{classroomId}','getAllReservationsByClassroom');
+    Route::middleware('jwt.verify:environment_remove')->get('/reservations/classroom/{classroomId}','getAllReservationsByClassroom');
 
     Route::middleware('sanitize:api')->middleware('jwt.verify:reservation_handling')->patch('/reservations/{reservationId}/reject', 'rejectReservation');
     Route::middleware('sanitize:api')->middleware('jwt.verify:reservation_handling')->patch('/reservations/{reservationId}/assign', 'assign');
-    Route::middleware('sanitize:api')->middleware('jwt.verify')->patch('/reservations/{reservationId}/cancel', 'cancelRequest');
+    Route::middleware('sanitize:api')->middleware('jwt.verify:reservation_cancel')->patch('/reservations/{reservationId}/cancel', 'cancelRequest');
 
     Route::middleware('sanitize:api')->middleware('jwt.verify:request_reserve')->post('/reservations', 'store');
 });
@@ -91,27 +96,25 @@ Route::controller(ClassroomStatusController::class)->group(function() {
 });
 
 Route::controller(ClassroomTypeController::class)->group(function() {
-    Route::middleware('jwt.verify')->get('/classrooms/types', 'list');
+    Route::middleware('jwt.verify:environment_register,environment_update')->get('/classrooms/types', 'list');
 });
+
 
 Route::controller(ClassroomController::class)->group(function() {
     Route::middleware('jwt.verify')->get('/classrooms', 'list');
-    Route::middleware('jwt.verify')->get('/classrooms/block/{blockId}','classroomsByBlock'); // ALL
+    Route::middleware('jwt.verify')->get('/classrooms/block/{blockId}','classroomsByBlock'); 
     Route::middleware('jwt.verify')->get('/classrooms/block/{blockId}/available', 'availableClassroomsByBlock');
     Route::middleware('jwt.verify')->get('/classrooms/last-validated', 'retriveLastClassroom');
-    Route::middleware('jwt.verify')->get('/classrooms/statistics/list','getAllClassroomsWithStatistics');
+    Route::middleware('jwt.verify:environment_remove')->get('/classrooms/statistics/list','getAllClassroomsWithStatistics');
 
-    Route::middleware('jwt.verify')->delete('/classrooms/delete/{classroomId}','destroy');
-
-    Route::group(['middleware' => ['sanitize:api','jwt.verify:request_reserve']], function() {
-        Route::post('/classrooms/reservation/suggest', 'suggestClassrooms');
-    });
-
-    Route::middleware('sanitize:api')->middleware('jwt.verify:availability')->post('/classrooms/disponibility', 'getClassroomByDisponibility');
-    Route::middleware('sanitize:api')->middleware('jwt.verify')->post('/classrooms', 'store');
+    Route::middleware('jwt.verify:environment_remove')->delete('/classrooms/delete/{classroomId}','destroy');
+    
+    Route::middleware('sanitize:api')->middleware('jwt.verify:request_reserve')->post('/classrooms/reservation/suggest', 'suggestClassrooms');
+    Route::middleware('sanitize:api')->middleware('jwt.verify')->post('/classrooms/disponibility', 'getClassroomByDisponibility');
+    Route::middleware('sanitize:api')->middleware('jwt.verify:environment_register')->post('/classrooms', 'store');
     Route::middleware('sanitize:api')->middleware('jwt.verify')->post('/classrooms/stats', 'getClassroomStats');
 
-    Route::middleware('sanitize:api')->middleware('jwt.verify')->put('/classrooms/{classroomId}', 'update');
+    Route::middleware('sanitize:api')->middleware('jwt.verify:environment_update')->put('/classrooms/{classroomId}', 'update');
 });
 
 //Agrupacion
@@ -129,8 +132,8 @@ Route::controller(NotificationController::class)->group(function() {
 
 Route::controller(BlockController::class)->group(function() {
     Route::middleware('jwt.verify')->get('/blocks', 'list');
-    Route::get('/blocks/{block_id}', 'show'); 
-    Route::get('/blocks/{block_id}/statistics', 'getBlockStatistics'); 
+    Route::middleware('jwt.verify')->get('/blocks/{block_id}', 'show'); 
+    Route::middleware('jwt.verify:block_remove')->get('/blocks/{block_id}/statistics', 'getBlockStatistics'); 
 
     Route::middleware('sanitize:api')->middleware('jwt.verify:block_register')->post('/blocks', 'store'); 
 
